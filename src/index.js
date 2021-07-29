97b840698215a687e5837064696a8fc0168342f8
olegilchuk <77920910+olegilchuk@users.noreply.github.com>
2021-07-29 17:56:54 +0200

    hw-10

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
 import creMenu from './teamlates/menu.hbs';
 import menu from './data/menu.json';
-creMenu(menu);
-console.log(creMenu(menu));
+
+const listRef = document.querySelector('.js-menu');
+const switchRef = document.querySelector('#theme-switch-toggle');
+const bodyRef = document.querySelector('body');
+
+const { LIGHT, DARK } = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+listRef.insertAdjacentHTML('beforeend', creMenu(menu));
+
+switchRef.addEventListener('change', changeTheme);
+
+let themeGet = localStorage.getItem('theme');
+console.log(themeGet);
+
+bodyRef.classList.add(themeGet ? themeGet : LIGHT);
+switchRef.checked = themeGet === DARK;
+function changeTheme(e) {
+  if (e.target.checked) {
+    bodyRef.classList.add(DARK);
+    bodyRef.classList.remove(LIGHT);
+    localStorage.setItem('theme', DARK);
+  } else {
+    bodyRef.classList.add(LIGHT);
+    bodyRef.classList.remove(DARK);
+    localStorage.setItem('theme', LIGHT);
+  }
+}
